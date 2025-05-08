602c7d1f09ad1036519437e525a12273a1771173
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-05-08 12:35:04 -0600

    Inicio del proyecto
    Se inicia el proyecto con la plantilla de ADC funcionales

---
+++ Proyecto2/Proyecto2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto2.c
+ *
+ * Created: 5/8/2025 11:29:27 AM
+ * Author : adria
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
