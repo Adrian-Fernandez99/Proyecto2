72ecc377185a72d9777bed9a6bc8593a21e87333
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-05-12 18:54:29 -0600

    Mapeo de servos
    Se realiza una función que mapea los servos de forma efectiva hacia la posiciones neecesarias para el funcinoamiento

--- Proyecto2/Proyecto2/main.c
+++ Proyecto2/Proyecto2/main.c
@@ -17,9 +17,10 @@ uso de dos potenciometros ditstintos.
 #include <avr/interrupt.h>
 
 // Prototipos de función
-void PWM_init();
 void setup();
+void PWM_init();
 void ADC_init();
+void mapeo_servo(uint8_t PWM_select, uint16_t ADC_servo1, uint16_t ADC_servo2, uint16_t* PWM_1, uint16_t* PWM_2);
 
 uint16_t ADC_read(uint8_t PIN);
 
@@ -38,8 +39,7 @@ int main(void)
 		ADC_servo1 = ADC_read(6);
 		ADC_servo2 = ADC_read(7);
 		
-		PWM_1 = (ADC_servo1 * 40000) / 1023 + 1000;
-		PWM_2 = (ADC_servo2 * 40000) / 1023 + 1000;
+		mapeo_servo(1, ADC_servo1, ADC_servo2, &PWM_1, &PWM_2);
 		
 		OCR1A = PWM_1;
 		OCR1B = PWM_2;
@@ -62,7 +62,7 @@ void setup()
 void PWM_init()
 {
 	DDRB |= (1 << PINB1) | (1 << PINB2);  // D9 y D10 como salida
-
+	
 	TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
 	TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);  // Modo Fast PWM 14, TOP = ICR1 y prescaler = 8
 
@@ -81,7 +81,41 @@ uint16_t ADC_read(uint8_t PIN)
 	ADMUX = (ADMUX & 0xF0) | (PIN & 0x0F);	// Selecciona pin de lectura
 	ADCSRA |= (1 << ADSC);                   // Inicia conversión
 	while (ADCSRA & (1 << ADSC));            // Espera a que termine
-	return ADCH;                              // Devuelve valor (10 bits)
+	return ADC;                              // Devuelve valor (10 bits)
+}
+
+void mapeo_servo(uint8_t PWM_select, uint16_t ADC_servo1, uint16_t ADC_servo2, uint16_t* PWM_1, uint16_t* PWM_2)
+{
+	if (PWM_select == 0)
+	{
+		if ((ADC_servo1 > 450) & (ADC_servo1 < 560))
+		{
+			(*PWM_1) = (490 * 4000UL) / 1023 + 1000;
+		}
+		else if (ADC_servo1 < 450)
+		{
+			(*PWM_1) = (380 * 4000UL) / 1023 + 1000;
+		}
+		else if (ADC_servo1 > 560)
+		{
+			(*PWM_1) = (570 * 4000UL) / 1023 + 1000;
+		}
+	}
+	else if (PWM_select == 1)
+	{
+		if ((ADC_servo2 > 450) & (ADC_servo2 < 560))
+		{
+			(*PWM_2) = (580 * 4000UL) / 1023 + 1000;
+		}
+		else if (ADC_servo1 < 450)
+		{
+			(*PWM_2) = (470 * 4000UL) / 1023 + 1000;
+		}
+		else if (ADC_servo1 > 560)
+		{
+			(*PWM_2) = (660 * 4000UL) / 1023 + 1000;
+		}
+	}
 }
 
 // Interrupt routines
